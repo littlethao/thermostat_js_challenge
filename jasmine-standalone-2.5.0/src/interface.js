c383b6971870f29aa49f45a5773a8c01b549bfbe
BenRoss92 <benjaminjackross@gmail.com>
2016-09-15 15:17:24 +0100

    playing around with jQuery

---
+++ jasmine-standalone-2.5.0/src/interface.js
@@ -0,0 +1,33 @@
+
+
+// $( document ).ready(function() {
+//
+//   // document.getElementById("increasetemp").innerHTML = thermostat.increaseTemperature;
+//
+//   // $( "button" ).addClass( "test" );
+//
+//   $( "#increasetemp" ).click(function( event ) {
+//
+//     event.preventDefault();
+//
+//     $( this ).hide( "slow" );
+//
+//
+// });
+//
+// $( "#decreasetemp" ).click(function( event ) {
+//
+//   event.preventDefault();
+//
+//   $( this ).hide( "slow" );
+//
+//
+// });
+//
+// });
+//
+// // window.onload = function() {
+// //
+// //     alert( "welcome" );
+// //
+// // };
